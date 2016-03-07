83a90c361dfe6f3ea3179f4ce228e911bf6e4a44
Xunnamius (Dynasty) <Xunnamius@users.noreply.github.com>
2016-03-07 14:43:03 -0600

    structural update

---
+++ cards.c
@@ -0,0 +1,30 @@
+/**
+ * This program evaluates face values. Released under the Vegas Public License.
+ *
+ * Lol. Whatever.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char card_name[3];
+    int val = 0;
+
+    puts("Enter the card name: ");
+    scanf("%2s", card_name);
+
+    if(card_name[0] == 'K' || card_name[0] == 'Q' || card_name[0] == 'J')
+        val = 10;
+
+    else if(card_name[0] == 'A')
+        val = 11;
+
+    else
+        val = atoi(card_name);
+
+    printf("The card value is: %i\n", val);
+
+    return 0;
+}
